972e8376e5f036067da82970682eb1a165b17f76
iremumut <82174470+iremumut@users.noreply.github.com>
2021-08-18 13:57:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import glob
 import requests
 import socket
+import subprocess
 from urllib.parse import urlparse
 
-path = "/etc/systemd/system/celery"  #change directory path
+path = "/etc/systemd/system/celery"  #change directory path #/etc/systemd/system/celery
 serviceFiles = glob.glob(path+"*.service")
-#dfsdfd
+
 def get_queue_names(service_files):
     queues = []
     for fl in service_files:
@@ -16,13 +17,15 @@ def get_queue_names(service_files):
                 if line.startswith("ExecStart"):
                     if (line.find('-Q') > 0):
                         queue_name = line.split('-Q')[1].split()[0]
-                        queues.append(queue_name)
+                        servicePath = fl.split("/")
+                        serviceName = servicePath[-1]
+                        queues.append(queue_name+","+serviceName)
                         #print(fl + "  -  " + queue_name)
     return queues
 
 
 def get_server_info():
-    server = open("/etc/conf.d/dopigo",'r') #change directory path
+    server = open("/etc/conf.d/dopigo",'r') #change directory path #/etc/conf.d/dopigo
     info = server.readlines()
     url = ""
     for line in info:
@@ -35,7 +38,8 @@ def get_server_info():
 
 
 def get_consumer_queues(server_url,ips):
-    response = requests.get(server_url+"api/consumers")
+    response = requests.get(server_url+"api/consumers")  ###UNCOMMENT LATER
+    #response = requests.get("http://guest:guest@localhost:15672/api/consumers")
     result = response.json()
     if response.status_code > 300:
         raise ValueError("Queue listesi alınamadı: {}".format(server_url))
@@ -57,7 +61,8 @@ def get_ip_addresses(hostname):
         raise ConnectionError("Could not connect to https://api.ipify.org")
     else: external = external.text
     s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    s.connect((hostname,80))
+    s.connect((hostname,80)) ##UNCOMMENT LATER
+    #s.connect(("8.8.8.8", 80))
     local = s.getsockname()[0]
     ips = [external,local]
     print("IP Adresses: ")
@@ -67,24 +72,45 @@ def get_ip_addresses(hostname):
     return ips
 
 def check_queues():
-    service_queues = get_queue_names(serviceFiles)
+    services_and_queues = get_queue_names(serviceFiles)
+    service_queues = []
+    service_names = []
+    for sr in services_and_queues:
+        names = sr.split(",")
+        service_queues.append(names[0])
+        service_names.append(names[1])
     url = get_server_info()
     hostname = urlparse(url).hostname
     queues_running = get_consumer_queues(url,get_ip_addresses(hostname))
     print("Queues from the services: ")
     print(service_queues)
+    print(service_names)
     print("Queues running: ")
     print(queues_running)
+    queues_not_found = []
     result = True
+    i = 0;
     for q in service_queues:
         if not(q in queues_running):
             print("Queue: "+q+" was not found")
+            queues_not_found.append(service_names[i])
             result = False
         else:
             print("Queue: "+q+" was found")
+        i+=1
     if(result):
         print("Success")
     else:
         print("Error")
+    print("Services that need to restart ")
+    print(queues_not_found)
+    return queues_not_found
 
-check_queues()
+def restart_services(services):
+    for service in services:
+        result = subprocess.run(["systemctl","start",service])
+        result.check_returncode()
+        print("Restarted the service"+service)
+
+
+restart_services(check_queues())
